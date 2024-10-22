6156147fdcb850a36032083408199c2a82f474dc
Tagore-7 <75824014+Tagore-7@users.noreply.github.com>
2024-10-22 11:47:54 -0400

    neetcode_daily_10/22/2024

---
+++ Hash_Usage.py
@@ -0,0 +1,8 @@
+names = ["alice", "brad", "collin", "brad", "dylan", "kim"]
+
+countMap = {}
+
+for name in names:
+    if name not in countMap:
+        countMap[name] = 0
+    countMap[namee] += 1
