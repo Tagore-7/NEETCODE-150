a05a302fc68b94dbd9ba6cb2d2c2d38a935adb45
Tagore-7 <75824014+Tagore-7@users.noreply.github.com>
2025-02-10 14:56:02 -0500

    neetcode_daily

---
+++ ImplementTriePrefix_Tree.py
@@ -0,0 +1,70 @@
+#A prefix tree (also known as a trie) is a tree data structure used to efficiently store and retrieve keys in a set of strings. Some applications of this data structure include auto-complete and spell checker systems.
+#
+#Implement the PrefixTree class:
+#
+#PrefixTree() Initializes the prefix tree object.
+#void insert(String word) Inserts the string word into the prefix tree.
+#boolean search(String word) Returns true if the string word is in the prefix tree (i.e., was inserted before), and false otherwise.
+#boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+#Example 1:
+#
+#Input: 
+#["Trie", "insert", "dog", "search", "dog", "search", "do", "startsWith", "do", "insert", "do", "search", "do"]
+#
+#Output:
+#[null, null, true, false, true, null, true]
+#
+#Explanation:
+#PrefixTree prefixTree = new PrefixTree();
+#prefixTree.insert("dog");
+#prefixTree.search("dog");    // return true
+#prefixTree.search("do");     // return false
+#prefixTree.startsWith("do"); // return true
+#prefixTree.insert("do");
+#prefixTree.search("do");     // return true
+#Constraints:
+#
+#1 <= word.length, prefix.length <= 1000
+#word and prefix are made up of lowercase English letters.
+#
+class TrieNode():
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for ch in word:
+            if ch not in curr.children:
+                curr.children[ch] = TrieNode()
+            curr = curr.children[ch]
+
+        curr.word = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for ch in word:
+            if ch not in curr.children:
+                return False
+            curr = curr.children[ch]
+
+        return curr.word
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for ch in prefix:
+            if ch not in curr.children:
+                return False
+            curr = curr.children[ch]
+
+        return True
+
+
+
+
